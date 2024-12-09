ee1532c419f3520039d14d5ee77e2df63c06d977
Salino3 <flaviobcn012@gmail.com>
2024-12-09 10:40:21 +0100

    add two functions

---
+++ src/components/payment-data/payment-data.js
@@ -0,0 +1,55 @@
+import { executingPage } from "../../js/app.js";
+
+function paymentInfoForm() {
+  const payment = {
+    cardNumber: document.getElementById("cardNumber").value,
+    cardHolderName: document.getElementById("cardHolderName").value,
+    expiryDate: document.getElementById("expiryDate").value,
+    cvv: document.getElementById("cvv").value,
+  };
+  console.log("Payment data:", payment);
+
+  const allFieldsFilled = Object.keys(payment).every((key) => payment[key]);
+
+  if (allFieldsFilled) {
+    localStorage.setItem("payment", JSON.stringify(payment));
+
+    window.location.href = "/src/components/payment-data/payment-data.html";
+  } else {
+    alert("Please refill all inputs.");
+  }
+}
+
+function loadTitle(h2Text) {
+  fetch("/src/common-app/title.component.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("main_page").innerHTML = data;
+
+      let h2 = document.getElementById("h2_title");
+
+      if (h2 && spanTitle) {
+        h2.textContent = h2Text;
+        h2.style.color = "var(--color-secondary)";
+        h2.style.fontSize = "var(--size-secondary)";
+      }
+    })
+    .catch((error) => console.error("Error downloading title:", error));
+}
+
+function loadPaymentData() {
+  fetch("./payment-data.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("main_page").innerHTML += data;
+    })
+    .catch((error) => console.error("Error downloading title:", error));
+}
+
+window.onload = function () {
+  executingPage()
+    .then(() => loadTitle("Insert payment data"))
+    .then(() => loadPaymentData())
+    //   .then(() => loadBoxButtons("Save"))
+    .catch((error) => console.error(error));
+};
