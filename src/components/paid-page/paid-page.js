b50d384b7abe0f2b74832225a54a9ee7e6b83ff2
Salino3 <flaviobcn012@gmail.com>
2024-12-14 10:29:57 +0100

    add first value

--- src/components/paid-page/paid-page.js
+++ src/components/paid-page/paid-page.js
@@ -1,7 +1,20 @@
 import { executingPage } from "../../js/app.js";
 
+function containerPaidPage() {
+  //   const main = document.getElementById("main_page").innerHTML;
+  //   const ul1 = document.getElementById("ul1");
+  console.log("Container Paid");
+  const li1 = document.getElementById("li1");
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  if (storedUser) {
+    if (li1) {
+      li1.textContent = `Name: ${storedUser.name}`;
+    }
+  }
+}
+
 window.onload = function () {
   executingPage()
-    .then(() => {})
+    .then(() => containerPaidPage())
     .catch((error) => console.error(error));
 };
