43287ecbe26771cee5b2bb3e40ccec16a80e2506
Salino3 <flaviobcn012@gmail.com>
2024-12-14 11:30:03 +0100

    change colors

--- src/components/paid-page/paid-page.js
+++ src/components/paid-page/paid-page.js
@@ -2,63 +2,63 @@ import { executingPage } from "../../js/app.js";
 
 function containerPaidPage() {
   const storedUser = JSON.parse(localStorage.getItem("user"));
-  const li1 = document.getElementById("li1");
-  const li2 = document.getElementById("li2");
-  const li3 = document.getElementById("li3");
-  const li4 = document.getElementById("li4");
+  const span1 = document.getElementById("span1");
+  const span2 = document.getElementById("span2");
+  const span3 = document.getElementById("span3");
+  const span4 = document.getElementById("span4");
   const storedAddress = JSON.parse(localStorage.getItem("address"));
-  const li5 = document.getElementById("li5");
-  const li6 = document.getElementById("li6");
-  const li7 = document.getElementById("li7");
-  const li8 = document.getElementById("li8");
+  const span5 = document.getElementById("span5");
+  const span6 = document.getElementById("span6");
+  const span7 = document.getElementById("span7");
+  const span8 = document.getElementById("span8");
   const storedPayment = JSON.parse(localStorage.getItem("payment"));
-  const li9 = document.getElementById("li9");
-  const li10 = document.getElementById("li10");
-  const li11 = document.getElementById("li11");
-  const li12 = document.getElementById("li12");
+  const span9 = document.getElementById("span9");
+  const span10 = document.getElementById("span10");
+  const span11 = document.getElementById("span11");
+  const span12 = document.getElementById("span12");
 
   if (storedUser) {
-    if (li1 && storedUser.name) {
-      li1.textContent = `Name: ${storedUser.name}`;
+    if (span1 && storedUser.name) {
+      span1.textContent = storedUser.name;
     }
-    if (li2 && storedUser.surname) {
-      li2.textContent = `Surname: ${storedUser.surname}`;
+    if (span2 && storedUser.surname) {
+      span2.textContent = storedUser.surname;
     }
-    if (li3 && storedUser.email) {
-      li3.textContent = `Email: ${storedUser.email}`;
+    if (span3 && storedUser.email) {
+      span3.textContent = storedUser.email;
     }
-    if (li4 && storedUser.phoneNumber) {
-      li4.textContent = `Phone number: ${storedUser.phoneNumber}`;
+    if (span4 && storedUser.phoneNumber) {
+      span4.textContent = storedUser.phoneNumber;
     }
   }
   //
   if (storedAddress) {
-    if (li5 && storedAddress.street) {
-      li5.textContent = `Street: ${storedAddress.street}`;
+    if (span5 && storedAddress.street) {
+      span5.textContent = storedAddress.street;
     }
-    if (li6 && storedAddress.city) {
-      li6.textContent = `City: ${storedAddress.city}`;
+    if (span6 && storedAddress.city) {
+      span6.textContent = storedAddress.city;
     }
-    if (li7 && storedAddress.country) {
-      li7.textContent = `Country: ${storedAddress.country}`;
+    if (span7 && storedAddress.country) {
+      span7.textContent = storedAddress.country;
     }
-    if (li8 && storedAddress.zipCode) {
-      li8.textContent = `Zip code: ${storedAddress.zipCode}`;
+    if (span8 && storedAddress.zipCode) {
+      span8.textContent = storedAddress.zipCode;
     }
   }
   //
   if (storedPayment) {
-    if (li9 && storedPayment.cardNumber) {
-      li9.textContent = `Card number: ${storedPayment.cardNumber}`;
+    if (span9 && storedPayment.cardNumber) {
+      span9.textContent = storedPayment.cardNumber;
     }
-    if (li10 && storedPayment.cardHolderName) {
-      li10.textContent = `Card holder name: ${storedPayment.cardHolderName}`;
+    if (span10 && storedPayment.cardHolderName) {
+      span10.textContent = storedPayment.cardHolderName;
     }
-    if (li11 && storedPayment.expiryDate) {
-      li11.textContent = `Expiry date: ${storedPayment.expiryDate}`;
+    if (span11 && storedPayment.expiryDate) {
+      span11.textContent = storedPayment.expiryDate;
     }
-    if (li12 && storedPayment.cvv) {
-      li12.textContent = `CVV: ${storedPayment.cvv}`;
+    if (span12 && storedPayment.cvv) {
+      span12.textContent = storedPayment.cvv;
     }
   }
 }
